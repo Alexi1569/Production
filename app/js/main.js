e195a9acc12b875d4fe8708f5433dce1e5f7acab
Alexi1569 <farikprof@gmail.com>
2018-08-20 09:19:34 +0300

    20.08.18

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,18 @@
 ;
 $(document).ready(function($) {
 	var windowWidth = $(window).width();
+
+	if (windowWidth <= 570) {
+		$('#mobile-menu').mmenu({
+			navbar: {
+				title: 'Красиво делай продакшн'
+			},
+			navbars: {
+				content : [ "close" ],
+			},
+			extensions: [
+        "theme-dark"
+     ]
+		});
+	}
 });
